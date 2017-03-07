32485e3060f1273ace48f7587af07ae432637596
hunterturner <hunter.turner@quibids.com>
2017-03-07 15:22:06 -0600

    Resized Employee Images, Updated History Text, Added Custom Image Slider

--- js/main.js
+++ js/main.js
@@ -57,242 +57,276 @@ $( document ).ready(function() {
         return false;
     });
 
+    //Slide Through Random Images
+    if ($('.default-header').length) {
+
+        var i = 2;
+        var int = setInterval(changeBG, 4000);
+        var slide1 = $(".default-header .slide-img");
+        var slide2 = $(".default-header .slide-img-2");
+
+        function changeBG() {
+            if (slide1.hasClass('header-hidden')) {
+                slide1.css('background-image', 'url("img/slideshow/' + ("0" + i).slice(-2) + '.jpg")');
+                slide1.removeClass('header-hidden');
+                window.setTimeout(function(){slide2.addClass('header-hidden');}, 300);
+            } else if (slide2.hasClass('header-hidden')) {
+                slide2.css('background-image', 'url("img/slideshow/' + ("0" + i).slice(-2) + '.jpg")');
+                slide2.removeClass('header-hidden');
+                window.setTimeout(function(){slide1.addClass('header-hidden');}, 300);
+            }
+
+
+            if(i == 13){
+                i= 1;
+            }
+            else{
+                i++;
+            }
+        }
+
+    }
+
     //Magnific Popups
-    $('#clearwater').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Clearwater/Clearwater-lg.jpg',
-                title: 'Clearwater'
+    if ($('#clearwater').length) {
+        $('#clearwater').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Clearwater/Clearwater-lg.jpg',
+                    title: 'Clearwater'
+                },
+                {
+                    src: 'img/homes/Clearwater/Clearwater-Back-lg.jpg',
+                    title: 'Clearwater Backyard'
+                },
+                {
+                    src: 'img/homes/Clearwater/Clearwater-Kitchen-lg.jpg',
+                    title: 'Clearwater Kitchen'
+                },
+                {
+                    src: 'img/homes/Clearwater/Clearwater-Living-lg.jpg',
+                    title: 'Clearwater Living Room'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Clearwater/Clearwater-Back-lg.jpg',
-                title: 'Clearwater Backyard'
+            type: 'image'
+        });
+        $('#continental').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Continental/Continental-lg.jpg',
+                    title: 'Continental'
+                },
+                {
+                    src: 'img/homes/Continental/Continental2-lg.jpg',
+                    title: 'Continental'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Clearwater/Clearwater-Kitchen-lg.jpg',
-                title: 'Clearwater Kitchen'
+            type: 'image'
+        });
+        $('#eastlake').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/EastLake/EastLake-lg.jpg',
+                    title: 'EastLake'
+                },
+                {
+                    src: 'img/homes/EastLake/EastLake2-lg.jpg',
+                    title: 'EastLake'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Clearwater/Clearwater-Living-lg.jpg',
-                title: 'Clearwater Living Room'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#continental').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Continental/Continental-lg.jpg',
-                title: 'Continental'
+            type: 'image'
+        });
+        $('#eastover').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/EastOver/EastOver-lg.jpg',
+                    title: 'EastOver'
+                },
+                {
+                    src: 'img/homes/EastOver/EastOver2-lg.jpg',
+                    title: 'EastOver'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Continental/Continental2-lg.jpg',
-                title: 'Continental'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#eastlake').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/EastLake/EastLake-lg.jpg',
-                title: 'EastLake'
+            type: 'image'
+        });
+        $('#founders').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Founders/Founders-lg.jpg',
+                    title: 'Founders'
+                },
+                {
+                    src: 'img/homes/Founders/Founders2-lg.jpg',
+                    title: 'Founders'
+                },
+                {
+                    src: 'img/homes/Founders/Founders-Back-lg.jpg',
+                    title: 'Founders Backyard'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/EastLake/EastLake2-lg.jpg',
-                title: 'EastLake'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#eastover').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/EastOver/EastOver-lg.jpg',
-                title: 'EastOver'
+            type: 'image'
+        });
+        $('#hollis').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Hollis/Hollis-lg.jpg',
+                    title: 'Hollis'
+                },
+                {
+                    src: 'img/homes/Hollis/Hollis-Kitchen-lg.jpg',
+                    title: 'Hollis Kitchen'
+                },
+                {
+                    src: 'img/homes/Hollis/Hollis-Living-lg.jpg',
+                    title: 'Hollis Living Room'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/EastOver/EastOver2-lg.jpg',
-                title: 'EastOver'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#founders').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Founders/Founders-lg.jpg',
-                title: 'Founders'
+            type: 'image'
+        });
+        $('#lakeview').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Lakeview/Lakeview-lg.jpg',
+                    title: 'Lakeview'
+                },
+                {
+                    src: 'img/homes/Lakeview/Lakeview-Back-lg.jpg',
+                    title: 'Lakeview Backyard'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Founders/Founders2-lg.jpg',
-                title: 'Founders'
+            type: 'image'
+        });
+        $('#legends').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Legends/Legends-lg.jpg',
+                    title: 'Legends'
+                },
+                {
+                    src: 'img/homes/Legends/Legends-Back-lg.jpg',
+                    title: 'Legends Backyard'
+                },
+                {
+                    src: 'img/homes/Legends/Legends-Kitchen-lg.jpg',
+                    title: 'Legends Kitchen'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Founders/Founders-Back-lg.jpg',
-                title: 'Founders Backyard'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#hollis').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Hollis/Hollis-lg.jpg',
-                title: 'Hollis'
+            type: 'image'
+        });
+        $('#stepping-stones').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Stepping-Stones/Stepping-Stones-lg.jpg',
+                    title: 'Stepping Stones'
+                },
+                {
+                    src: 'img/homes/Stepping-Stones/Stepping-Stones-Bathroom-lg.jpg',
+                    title: 'Stepping Stones Bathroom'
+                },
+                {
+                    src: 'img/homes/Stepping-Stones/Stepping-Stones-Kitchen-lg.jpg',
+                    title: 'Stepping Stones Kitchen'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Hollis/Hollis-Kitchen-lg.jpg',
-                title: 'Hollis Kitchen'
+            type: 'image'
+        });
+        $('#valley-forge').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Valley-Forge/Valley-Forge-lg.jpg',
+                    title: 'Valley Forge'
+                },
+                {
+                    src: 'img/homes/Valley-Forge/Valley-Forge-Living-lg.jpg',
+                    title: 'Valley Forge Living Room'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Hollis/Hollis-Living-lg.jpg',
-                title: 'Hollis Living Room'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#lakeview').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Lakeview/Lakeview-lg.jpg',
-                title: 'Lakeview'
+            type: 'image'
+        });
+        $('#walnut-creek').magnificPopup({
+            items: [
+                {
+                    src: 'img/homes/Walnut-Creek/Walnut-Creek-lg.jpg',
+                    title: 'Walnut Creek'
+                },
+                {
+                    src: 'img/homes/Walnut-Creek/Walnut-Creek-Back-lg.jpg',
+                    title: 'Walnut Creek Backyard'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Lakeview/Lakeview-Back-lg.jpg',
-                title: 'Lakeview Backyard'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#legends').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Legends/Legends-lg.jpg',
-                title: 'Legends'
+            type: 'image'
+        });
+    }
+    if ($('#culpepper-creek').length) {
+        $('#culpepper-creek').magnificPopup({
+            items: [
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm2.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm3.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm4.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm7.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm8.jpg',
+                    title: 'Culpepper Creek Farm'
+                },
+                {
+                    src: 'img/CulpepperCreek/Culpepper-Creek-Farm9.jpg',
+                    title: 'Culpepper Creek Farm'
+                }
+            ],
+            gallery: {
+                enabled: true
             },
-            {
-                src: 'img/homes/Legends/Legends-Back-lg.jpg',
-                title: 'Legends Backyard'
-            },
-            {
-                src: 'img/homes/Legends/Legends-Kitchen-lg.jpg',
-                title: 'Legends Kitchen'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#stepping-stones').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Stepping-Stones/Stepping-Stones-lg.jpg',
-                title: 'Stepping Stones'
-            },
-            {
-                src: 'img/homes/Stepping-Stones/Stepping-Stones-Bathroom-lg.jpg',
-                title: 'Stepping Stones Bathroom'
-            },
-            {
-                src: 'img/homes/Stepping-Stones/Stepping-Stones-Kitchen-lg.jpg',
-                title: 'Stepping Stones Kitchen'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#valley-forge').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Valley-Forge/Valley-Forge-lg.jpg',
-                title: 'Valley Forge'
-            },
-            {
-                src: 'img/homes/Valley-Forge/Valley-Forge-Living-lg.jpg',
-                title: 'Valley Forge Living Room'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#walnut-creek').magnificPopup({
-        items: [
-            {
-                src: 'img/homes/Walnut-Creek/Walnut-Creek-lg.jpg',
-                title: 'Walnut Creek'
-            },
-            {
-                src: 'img/homes/Walnut-Creek/Walnut-Creek-Back-lg.jpg',
-                title: 'Walnut Creek Backyard'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
-    $('#culpepper-creek').magnificPopup({
-        items: [
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm2.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm3.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm4.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm7.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm8.jpg',
-                title: 'Culpepper Creek Farm'
-            },
-            {
-                src: 'img/CulpepperCreek/Culpepper-Creek-Farm9.jpg',
-                title: 'Culpepper Creek Farm'
-            }
-        ],
-        gallery: {
-            enabled: true
-        },
-        type: 'image'
-    });
+            type: 'image'
+        });
+    }
     
 });
